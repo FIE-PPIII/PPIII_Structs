0da8dda14f119b645a67ec7ac66b97dcc75766b7
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-08-18 22:00:31 -0300

    MOD - testing List in main

--- main.cpp
+++ main.cpp
@@ -6,7 +6,14 @@ using namespace std;
 int main()
 {
     cout<<"Hello, world!"<<endl;
-    Node<int> node(7);
-    cout<<node.getValue()<<endl;
+    List<int> list;
+    cout<<"Length: "<<list.length()<<endl;
+    list.append(1);
+    list.append(2);
+    cout<<"Length: "<<list.length()<<endl;
+    cout<<list.pop()<<endl;
+    cout<<list.pop()<<endl;
+    cout<<list.pop()<<endl;
+    cout<<"Length: "<<list.length()<<endl;
     return 0;
 }
